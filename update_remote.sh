dd72a8911a7c958c3235e08e13679551cb6dcfeb
dengjiayue <3508225629@qq.com>
2023-05-28 20:37:38 +0800

    update: Auto commit And push

--- update_remote.sh
+++ update_remote.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/expect -f
 set timeout 
-set cmd1 "cd ~/app/blogs && git pull origin master:master"
-# 使用公钥登录不需要密码
+set cmd1 "cd ~/app/blogs && git pull origin master:master "
+# 使用公钥登录不需要密码 
 spawn ssh -p 22 djy@djy.bmft.tech
 expect {
     # 发送更新命令
-    "*#" {send "$cmd1\r"}
+    "*" {send "$cmd1\r"}
 }
 interact
